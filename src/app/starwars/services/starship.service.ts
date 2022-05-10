bc9fed6cd0f38f113b610c94677c77f7b79da284
davidsantander9@gmail.com <davidsantander9@gmail.com>
2022-05-09 23:19:54 -0500

    add comments to starship service

--- src/app/starwars/services/starship.service.ts
+++ src/app/starwars/services/starship.service.ts
@@ -5,26 +5,57 @@ import { environment } from 'src/environments/environment';
 import { Movie, myStarship, Starship, StarwarsResponse } from '../interfaces/starwars.interfaces';
 
 
+/**
+ * Description: 
+ *  - Provides CRUD starship methods
+ */
+
 @Injectable({
   providedIn: 'root'
 })
 export class StarshipService {
+  /** baseUrl: base url starhip api  */
   private baseUrl: string = environment.baseHerokuStarWarsUrl;
   
+  /**
+   * 
+   * @param http Injectable class, with methods to perform HTTP request
+   */
   constructor(private http: HttpClient ) { }
 
+  /**
+   * get and starhip
+   * @param id starship identifier
+   * @returns Observable of starship list
+   */
   getStarshipByIdApi( id: string ): Observable<myStarship[]> {
     return this.http.get<myStarship[]>(`${ this.baseUrl }/starships/?id_api=${id}`);
   }
 
+  /**
+   * create starship
+   * @param id starship identifier
+   * @returns Observable of starship
+   */
   createStarship(starship: Starship): Observable<myStarship>{
     return this.http.post<myStarship>(`${ this.baseUrl }/starships/`, starship);
   }
 
+  /**
+   * update starship
+   * @param starship starhip info
+   * @param id starship identifier
+   * @returns Observable of starship
+   */
   updateStarship(starship: Starship, id: number): Observable<myStarship>{
     return this.http.put<myStarship>(`${ this.baseUrl }/starships/${id}/`, starship);
   }
 
+  /**
+   * delete starhip
+   * @param id starship identifier
+   * @returns Observable
+   */
   deleteStarship(id: number): Observable<any>{
     return this.http.delete<any>(`${ this.baseUrl }/starships/${id}/`);
   }
